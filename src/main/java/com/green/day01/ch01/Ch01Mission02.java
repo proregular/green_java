374387987322464b8008a85d434423a338b96a13
SugiSagong <jumoney1012@gmail.com>
2024-10-07 17:27:13 +0900

    first

---
+++ src/main/java/com/green/day01/ch01/Ch01Mission02.java
@@ -0,0 +1,7 @@
+package com.green.day01.ch01;
+
+public class Ch01Mission02 {
+    public static void main(String[] args) {
+        System.out.println("2 * 5 = " + 2* 5);
+    }
+}
---
+++ src/main/java/com/green/day01/ch03/Constants.java
@@ -0,0 +1,15 @@
+package com.green.day01.ch03;
+
+public class Constants {
+    public static void main(String[] args) {
+        final int MAX_SIZE = 100;
+        final char CONST_CHAR = '상';
+        final int CONST_ASSIGNED;
+
+        CONST_ASSIGNED = 12;
+
+        System.out.printf("상수1: %d\n", MAX_SIZE);
+        System.out.printf("상수2: %c\n", CONST_CHAR);
+        System.out.printf("상수3: %d\n", CONST_ASSIGNED);
+    }
+}
